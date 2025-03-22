794dfc40c841171df2bede1fc7bdff7694978eb4
mokshit <mokj0802@gmail.com>
2025-03-22 21:51:46 +0800

    frontend connect

--- main.py
+++
@@ -1,6 +0,0 @@
-from change_assistant import run_change_assistant
-
-if __name__ == "__main__":
-    tech = input("Enter the technology to adopt: ")
-    framework = input("Enter the change framework (e.g. ADKAR): ")
-    run_change_assistant(tech, framework)
